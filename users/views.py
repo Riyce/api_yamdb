90ace4411455af22ce6f2281027b7e0201a49896
Yauheni <smilodon2@yandex.ru>
2021-02-17 22:00:12 +0300

    добавил вид obtain token для переопределения получения токена

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from drfpasswordless.views import AbstractBaseObtainAuthToken
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import CallbackTokenAuthSerializer
+
+
+class ObtainToken(AbstractBaseObtainAuthToken):
+    permission_classes = (AllowAny,)
+    serializer_class = CallbackTokenAuthSerializer
+
