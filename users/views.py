ccc4cde8da5fbbb7f74b2e4448cd4f13eca4528a
Yauheni <smilodon2@yandex.ru>
2021-02-18 08:45:49 +0300

    передел получение токена аутентификации определили viewset для работы с пользователями

--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from drfpasswordless.views import AbstractBaseObtainAuthToken
 from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
 
-from .serializers import CallbackTokenAuthSerializer
+from .serializers import CallbackTokenAuthSerializer, UserProfileSerializer
 
 
 class ObtainToken(AbstractBaseObtainAuthToken):
     permission_classes = (AllowAny,)
     serializer_class = CallbackTokenAuthSerializer
 
+
+class UserConfigViewSet(viewsets.ModelViewSet):
+    serializer_class = UserProfileSerializer
