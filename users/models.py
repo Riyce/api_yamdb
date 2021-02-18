8064c97d7ff72a7a954bdf7c0854d0315eee0ef9
Yauheni <smilodon2@yandex.ru>
2021-02-18 08:47:44 +0300

    определили модель данных пользователя переопределил модели для аддона passwordless

--- users/models.py
+++ users/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    pass
+class UserProfile(models.Model):
+
+    class Role(models.TextChoices):
+        USER = 'User'
+        MODERATOR = 'Moderator'
+        ADMIN = 'Admin'
+        DJANGO_ADMIN = 'Django_admin'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name="profile")
+    description = models.TextField(blank=True, null=True)
+    role = models.TextField(choices=Role.choices, default='User')
+
+    def __str__(self):
+        return self.user.username
