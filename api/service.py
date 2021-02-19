1773fd1c65cde48504270187fa26ce06a99ae8ad
Riyce <riyce@mail.ru>
2021-02-19 18:47:14 +0300

    Title filter

--- api/service.py
+++ api/service.py
@@ -1,3 +1,23 @@
 from django_filters import rest_framework as filters
 
+from .models import Title
 
+
+class TitleFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains'
+    )
+    year = filters.NumberFilter(lookup_expr='exact')
+    genre = filters.CharFilter(
+        field_name='genre__slug',
+        lookup_expr='icontains'
+    )
+    category = filters.CharFilter(
+        field_name='category__slug',
+        lookup_expr='icontains'
+    )
+
+    class Meta:
+        model = Title
+        fields = ['genre', 'category', 'year', 'name']
