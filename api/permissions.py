546b395606978a2000ee2a4d43232ae21e17f10b
Riyce <riyce@mail.ru>
2021-02-19 18:53:16 +0300

    try to make perm

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions
 
+
 #class IsAdminOrReadOnly(permissions.BasePermission):
     #def has_object_permission(self, request, view, obj):
-        #if request.method not in permissions.SAFE_METHODS:
-            #return request.user.is_admin
+        #if request.method != 'GET':
+            #return request.user.profile__role == 'Admin'
         #return True
