15f883cae4936463c848b6355b782ae494514b3e
Yauheni <smilodon2@yandex.ru>
2021-02-20 19:27:27 +0300

    прописал енд поинты для работы с токенами и пользователями

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,6 @@ urlpatterns = [
          as_view(template_name='redoc.html'), name='redoc'),
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/users/', include('users.urls')),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
