0ee7b60a26dd5d54ea34317f31fc500608008bba
Yauheni <smilodon2@yandex.ru>
2021-02-20 19:20:20 +0300

    добавил ендпопониты взаимодействия с ползователями

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include, re_path
-from .views import ObtainToken, UserConfigViewSet
+from .views import ObtainToken, UserViewSet, UserUpdateAPIView
 from drfpasswordless.views import ObtainEmailCallbackToken
-from django.views.decorators.csrf import csrf_exempt
+
+user_router = DefaultRouter()
+
+user_router.register('', UserViewSet, basename='User')
 
 urlpatterns = [
     path('token/', ObtainToken.as_view(), name='obtain_token'),
     path('email/', ObtainEmailCallbackToken.as_view(), name='obtain_email'),
-    path('me/', csrf_exempt(UserConfigViewSet), name='user_config')
+    path('me/', UserUpdateAPIView.as_view(), name='update_user'),
+    path('', include(user_router.urls))
 ]
