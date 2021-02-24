00e2454e329983e15efa33e624870e55efc2807e
Yauheni <smilodon2@yandex.ru>
2021-02-24 21:34:57 +0300

    поправил енд поинты удалил лишние импорты

--- users/urls.py
+++ users/urls.py
@@ -2,15 +2,14 @@ from django.urls import path, include
 from drfpasswordless.views import ObtainEmailCallbackToken
 from rest_framework.routers import DefaultRouter
 
-from .views import ObtainToken, UserViewSet, UserUpdateAPIView
+from .views import ObtainToken, UserViewSet
 
 user_router = DefaultRouter()
 
-user_router.register('', UserViewSet, basename='User')
+user_router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
     path('token/', ObtainToken.as_view(), name='obtain_token'),
     path('email/', ObtainEmailCallbackToken.as_view(), name='obtain_email'),
-    path('me/', UserUpdateAPIView.as_view(), name='update_user'),
     path('', include(user_router.urls))
 ]
