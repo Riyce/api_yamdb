e2f82361dc7bc332bb15a2d7f00f4df10e5fc963
Riyce <riyce@mail.ru>
2021-02-25 00:12:20 +0300

    After review

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('api/', include('api.urls')),
@@ -11,11 +10,4 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    #path('api/v1/auth/', include('users.urls')),
-    #path('api/v1/users/', include('users.urls')),
-    #path(
-    #    'api/v1/token/refresh/',
-    #    TokenRefreshView.as_view(),
-    #    name='token_refresh'
-    #),
 ]
